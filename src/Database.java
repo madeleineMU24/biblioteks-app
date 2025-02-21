7302eb90074255df7113fa3153a380da4302a437
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2025-02-21 09:46:23 -0800

    password and user fix

--- src/Database.java
+++ src/Database.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public class Database {
 
     public static final String url = "jdbc:mysql://localhost:3306/bibliotekapp";
-    public static final String user = "root";
-    public static final String password = "pass123";
+    public static final String user = System.getenv("MYSQL_USER");
+    public static final String password = System.getenv("MYSQL_PASSWORD");
 
     public static Connection getConnection() throws SQLException{
         Connection conn = DriverManager.getConnection(url, user, password);
